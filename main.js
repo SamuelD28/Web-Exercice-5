632d90d01703e94b6ae94fc8ea5145b9c3905c85
Charles Oligny <1951796@cstjean.qc.ca>
2019-10-30 15:24:46 -0400

    Add : Todo list

--- main.js
+++ main.js
@@ -1,1 +1,31 @@
-alert("ok");
+
+/**
+ * Todo : 
+ * -Spawn un dequoi en haut
+ * -Bouge lobject vers le bas. Vitesse constante
+ * -Respecter les limites de lecrans
+ * -Spawn a un intervalle de temps
+ * -Spawn aleatoire
+ * -Systeme de score
+ * -Acceleration de la partie
+ * 
+ * -Creer un zombie 
+ *      -90% de spawn
+*       -score + 10
+        -domage - 20
+        -Detruire »: hover avec souris
+ * -Creer un coffin
+ *      -10% de spawn
+ *      -Score + 50
+ *      -Domage -200
+ *      -Detruire : Double click avec souris
+ * 
+ * -Decrementer arriver au bas
+ * -Destruction fade out
+ * -Partie termine. Fade out des objects qui ont pas atteint le sol
+ * -Message game over
+ * -Bloquer le clic contextuel
+ * -Ajuster pour un ecran qui se redimenssione
+ * -FUsée qui suit la souris
+ * 
+ */
