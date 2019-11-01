4f66010a60673dc1a09341719804cff3b7e73b1f
1489005 <1489005@cstjean.qc.ca>
2019-11-01 11:49:44 -0400

    Wip : Refactor and spawn logic

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 
 /**
+ * BUG : le width() et le height() retourne zero
+ * 
+ * 
  * Todo : 
  * -Spawn a un intervalle de temps
  * -Spawn aleatoire
@@ -46,22 +49,34 @@ class Game{
 
     start(){
         setInterval(()=>{
-            this.spawn();
-            this.moveZombie();
+            let zombie = this.createZombie();
+            this.createInCanvas(zombie);
+            zombie.moveToBottom(this.canvasHeight);
         }, 1000);
     }
     
-    spawn(){
-        this.canvas.append("<div class='zombie'>🧟</div>");
-        let zombie = $(".zombie");
-        let leftPositionSpawn = Math.max(Math.random() * this.canvasWidth - zombie.width(), 0);
-        zombie.css({"left" : leftPositionSpawn + "px"});
+    createZombie(){
+        let zombie = new Zombie();
+        zombie.setLeftPosition(this.PickRandomZombieSpawnPoint(zombie));
+        return zombie;
+    }
+
+    createInCanvas(zombie){
+        this.canvas.append(zombie.element);
     }
 
     moveZombie() {
         let zombie = $(".zombie");
         let zombieHeight = zombie.height();
         zombie.animate({ "top": this.canvasHeight - zombieHeight }, 2000);
+    }
+
+    PickRandomZombieSpawnPoint(zombie){
+        return Math.max(this.PickRandomLeftPositionInCanvas() - zombie.element.width(), 0);
+    }
+
+    PickRandomLeftPositionInCanvas(){
+        return Math.random() * this.canvasWidth;
     }
 
     stop(){
@@ -77,7 +92,17 @@ class Game{
 
 class Zombie{
 
-    spawn(){
-        let zombie = "<div class='zombie'>🧟</div>";
+    constructor(){
+        this.element = $("<div>🧟</div>")
+                        .css({left : 0 + "px"})
+                        .addClass("zombie");
+    }
+
+    setLeftPosition(left){
+        this.element.css({left : left + "px"});
+    }
+
+    moveToBottom(bottom){
+        this.element.animate({ "top": bottom - this.element.height() }, 2000);
     }
 }
