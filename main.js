c8412af84952021275df5561a9adc5b71fe70baa
samueldube <samuel_personnel@outlook.com>
2019-11-01 20:07:59 -0400

    Add : http server for javascript module

--- main.js
+++ main.js
@@ -32,8 +32,7 @@
  * 
  */
 
-
-// "<div class="zombie">🧟</div>"
+import Enemy from "./Enemy.js";
 
 (function() {
     window.addEventListener("DOMContentLoaded", (event) => {
@@ -102,56 +101,5 @@ class Game{
     end() {
 
 
-    }
-}
-
-class Enemy {
-
-    constructor() {
-        this.element = this.enemyFactory();
-        this.id = this.setId();
-        this.setElementProperty(this.id);
-    }
-
-    setLeftPosition(left) {
-        this.element.css({left : left + "px"});
-    }
-
-    moveToBottom(bottom) {
-        this.element.animate({ "top": bottom - this.element.height() }, 2000);
-    }
-
-    enemyFactory() {
-        let rdn = Math.floor(Math.random() * 10) + 1;
-        if (rdn == 10) {
-            return $("<div>⚰️</div>")
-            .css({left : 0 + "px"})
-            .addClass("coffin")
-            .addClass("enemy")
-        } else {
-            return $("<div>🧟</div>")
-            .css({left : 0 + "px"})
-            .addClass("zombie")
-            .addClass("enemy")
-        }
-    }
-
-    setId() {
-        window.enemyCount++;
-        this.element.attr("id", window.enemyCount);
-        return window.enemyCount; 
-    }
-
-    setElementProperty(id) {
-        console.log(id);
-        console.log(this.id);
-        // let str = "#" + id;
-        // console.log(str);
-
-        $("#" + id).mouseenter(function() {
-            console.log("yeah");
-
-            $("#" + this.id).fadeOut();
-        });
     }
 }
